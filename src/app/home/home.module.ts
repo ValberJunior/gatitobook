ad482bc560c61d4dd0e7540858aa41bb4dbb2dcf
ValberJunior <valberjr1992@gmail.com>
2022-10-01 18:05:02 -0300

    create home and login screen

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ],
+  //para poder expor o componente
+  exports: [
+    HomeComponent
+  ]
+})
+export class HomeModule { }
