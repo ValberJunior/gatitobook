7e39f6f0bdb65411775115a880a2770ece2180c1
ValberJunior <valberjr1992@gmail.com>
2022-10-01 23:35:39 -0300

    add httpclientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     //importar os modules dos componentes que criamos ou queremos adc
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
