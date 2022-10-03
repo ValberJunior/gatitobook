08100414c3f458b027c8e793f1d2488e91a45c93
ValberJunior <valberjr1992@gmail.com>
2022-10-03 11:14:17 -0300

    create header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/authentication/user/user.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private userService : UserService, private router : Router) { }
+
+  user$ = this.userService.returnUser();
+
+  ngOnInit(): void {
+  }
+
+  logout(){
+    this.userService.Logout();
+    this.router.navigate(['']);
+  }
+
+}
