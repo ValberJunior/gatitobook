d65cd45d5c88e79edfac931609b381cb9b54c7ac
ValberJunior <valberjr1992@gmail.com>
2022-10-03 10:54:04 -0300

    increment interface with user service

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -1,7 +1,8 @@
+import { UserService } from './user/user.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { ISignIn } from 'src/interfaces';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,18 @@ export class AuthService {
       password:""
     }
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(
+    private httpClient : HttpClient,
+    private userService : UserService) { }
 
-  authenticate(credential :ISignIn):Observable<any>{
-    return this.httpClient.post('http://localhost:3000/user/login',credential)
+  authenticate(credential :ISignIn):Observable<HttpResponse<any>>{
+    return this.httpClient.post('http://localhost:3000/user/login',credential,
+    { observe : 'response'}
+    ).pipe(
+      tap((res)=>{
+        const authToken = res.headers.get('x-access-token') ?? '';
+        this.userService.saveToken(authToken);
+      })
+    )
   }
 }
