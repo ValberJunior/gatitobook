83fc389b6b2b78ecac706a28dc8140d8bc676220
ValberJunior <valberjr1992@gmail.com>
2022-10-03 10:56:00 -0300

    code review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { HeaderModule } from './components/header/header.module';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FooterModule } from './components/footer/footer.module';
 
 @NgModule({
   declarations: [
