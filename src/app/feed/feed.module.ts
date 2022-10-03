67056afba3b3c98d8a7e8affa63196bc31a3330f
ValberJunior <valberjr1992@gmail.com>
2022-10-03 14:12:24 -0300

    add Card and Gridphotos component in declarations

--- src/app/feed/feed.module.ts
+++ src/app/feed/feed.module.ts
@@ -6,18 +6,22 @@ import { CommonModule } from '@angular/common';
 import { FeedRoutingModule } from './feed-routing.module';
 import { ListComponent } from './list/list.component';
 import { CardImageComponent } from './card-image/card-image.component';
+import { CardModule } from '../components/card/card.module';
+import { GridPhotosComponent } from './grid-photos/grid-photos.component';
 
 
 @NgModule({
   declarations: [
     ListComponent,
-    CardImageComponent
+    CardImageComponent,
+    GridPhotosComponent
   ],
   imports: [
     CommonModule,
     FeedRoutingModule,
     HeaderModule,
-    FooterModule
+    FooterModule,
+    CardModule
   ]
 })
 export class FeedModule { }
