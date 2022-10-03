0a5d4bbe1f0a20ed20817cf1c42d850224a6a908
ValberJunior <valberjr1992@gmail.com>
2022-10-03 14:13:23 -0300

    add user and items service -- and update component

--- src/app/feed/list/list.component.ts
+++ src/app/feed/list/list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserService } from 'src/app/authentication/user/user.service';
+import { Items } from 'src/utils';
+import { ItemsService } from '../items.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  items! : Items;
+
+  constructor(private userService : UserService,
+              private itemsService : ItemsService
+              ) { }
 
   ngOnInit(): void {
+    this.userService.returnUser().subscribe(
+      user => {
+        const userName = user.name ?? '';
+        this.itemsService.userList(userName).subscribe(
+          (items) => {
+            this.items = items;
+          }
+        )
+      }
+    )
   }
 
 }
