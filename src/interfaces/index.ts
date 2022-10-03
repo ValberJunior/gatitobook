f1a176d53ea70d1d04238278bf8e06da05294bcb
ValberJunior <valberjr1992@gmail.com>
2022-10-03 11:44:28 -0300

    code review and add new interface -- IItem

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,17 +1,28 @@
 export interface ISignIn{
-  userName: string,
+  userName: string;
   password: string
 }
 
 export interface INewUser{
-  userName: string,
-  email: string,
-  fullName: string,
+  userName: string;
+  email: string;
+  fullName: string;
   password: string
 }
 
 export interface IUser{
   id?: number;
-  name?:string,
+  name?:string;
   email?:string
+}
+
+export interface IItem{
+  id:number;
+  postDate: Date;
+  url: string;
+  description: string;
+  allowComments: boolean;
+  likes: number;
+  comments: number;
+  userId: number
 }
