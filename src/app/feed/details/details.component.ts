ac29967c6d48f4fbf40ad89144a69b595edff649
ValberJunior <valberjr1992@gmail.com>
2022-10-04 10:16:39 -0300

    create Details component

--- src/app/feed/details/details.component.ts
+++ src/app/feed/details/details.component.ts
@@ -1,4 +1,8 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { IItem } from 'src/interfaces';
+import { ItemsService } from '../items.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  itemId! : number;
+  item$! : Observable<IItem>;
+
+  constructor(
+                private itemsService : ItemsService,
+                private activatedRoute : ActivatedRoute    // com esse serviço tenho acesso ao meu id
+  ) { }
 
   ngOnInit(): void {
+    this.itemId = this.activatedRoute.snapshot.params['id']; //check
+    this.item$ = this.itemsService.searchById(this.itemId);
   }
 
 }
