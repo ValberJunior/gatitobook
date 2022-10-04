7873e699309fed45bd6f0ae7db1cd49f47c7631c
ValberJunior <valberjr1992@gmail.com>
2022-10-04 10:15:51 -0300

    add isLogged method

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/authentication/user/user.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService : UserService) { }
 
   ngOnInit(): void {
+  }
+
+  isLogged(): boolean{
+    if(this.userService.isLogged()) return true;
+    return false;
   }
 
 }
